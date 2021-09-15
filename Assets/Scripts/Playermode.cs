503c816b85529acc402639ee4daaec4acd457a7a
Malvarez97 <maurialva.ma@gmail.com>
2021-09-15 15:51:41 -0300

    primer commit

---
+++ Assets/Scripts/Playermode.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermode : MonoBehaviour
+{
+    public float velocityMov = 5.0f;
+    public float velocityRot = 200.0f;
+    private Animator anim;
+    public float x, y; // como se mueve el personaje ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        transform.Rotate(0, x * Time.deltaTime * velocityRot, 0);
+        transform.Translate(0, 0, y * Time.deltaTime * velocityMov);
+        anim.SetFloat("VelX", x);
+        anim.SetFloat("VelY", y);
+    }
+}
